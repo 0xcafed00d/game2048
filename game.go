c5ea193d9a342e94f28d7eb04a8f2030d7fd82c7
Lee Witek <neomech@gmail.com>
2015-06-30 01:20:02 +0100

    building blitter

--- game.go
+++ game.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/nsf/termbox-go"
-	//"github.com/simulatedsimian/rect"
+	"github.com/simulatedsimian/rect"
 )
 
 type Buffer interface {
@@ -62,6 +62,35 @@ func (b *MemBuffer) GetCell(x, y int) (rune, uint16, uint16) {
 
 func (b *MemBuffer) Size() (int, int) {
 	return b.w, b.h
+}
+
+const (
+	CHAR    = 1
+	FG      = 2
+	BG      = 4
+	ALL     = CHAR | FG | BG
+	ATTRIBS = FG | BG
+)
+
+func FillArea(dst Buffer, area rect.Rectangle, ch rune, fg, bg uint16, filltype int) {
+	area, ok := rect.Intersection(rect.WH(dst.Size()), area)
+	if !ok {
+	}
+}
+
+func Fill(dst Buffer, ch rune, fg, bg uint16, filltype int) {
+	FillArea(dst, rect.WH(dst.Size()), ch, fg, bg, filltype)
+}
+
+func BlitBuffer(src, dst Buffer, xpos, ypos int) {
+	srcSz := rect.WH(src.Size())
+	srcSz.Translate(rect.Vec{xpos, ypos})
+
+	dstSz := rect.WH(dst.Size())
+
+	if intersect, ok := rect.Intersection(srcSz, dstSz); ok {
+		_ = intersect
+	}
 }
 
 func main() {
