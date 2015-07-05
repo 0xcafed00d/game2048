6cd41012f14b86d422199adbeacc890e07873cfb
Lee Witek <neomech@gmail.com>
2015-07-05 23:18:45 +0100

    convert test to use gamecore

--- game.go
+++ game.go
@@ -3,83 +3,50 @@ package main
 import (
 	"github.com/nsf/termbox-go"
 	"github.com/simulatedsimian/rect"
-	"time"
 )
 
 func main() {
 	gc := GameCore{}
 
-	gc.OnInit = nil
-	gc.OnEvent = nil
-	gc.OnTick = nil
-
-	err := gc.Start()
-	if err != nil {
-		panic(err)
-	}
-}
-
-func mainx() {
-	err := termbox.Init()
-	if err != nil {
-		panic(err)
-	}
-	defer termbox.Close()
-
-	eventQueue := make(chan termbox.Event)
-	go func() {
-		for {
-			eventQueue <- termbox.PollEvent()
-		}
-	}()
-
-	tbuffer := TermboxBuffer{}
-
-	r := rect.WH(tbuffer.Size())
-	r.Expand(rect.Vec{-1, -1})
-	FillArea(tbuffer, r, 'x', termbox.ColorCyan, termbox.ColorGreen, CHAR)
+	var r rect.Rectangle
+	x, y := 0, 0
+	dx, dy := 1, 1
 
 	b := MakeMemBuffer(10, 10)
 	Fill(b, '0', termbox.ColorRed, termbox.ColorBlue, ALL)
 
-	BlitBuffer(b, tbuffer, 1, 1)
+	gc.OnInit = func(gc *GameCore) error {
+		r = rect.WH(gc.BackBuffer.Size())
+		r.Expand(rect.Vec{-1, -1})
+		return nil
+	}
 
-	termbox.Flush()
-
-	doQuit := false
-
-	ticker := time.NewTicker(time.Millisecond * 10)
-
-	x, y := 0, 0
-	dx, dy := 1, 1
-
-	for !doQuit {
-		select {
-		case ev := <-eventQueue:
-			if ev.Type == termbox.EventKey {
-				if ev.Ch == 'q' {
-					doQuit = true
-				}
-				termbox.Flush()
-			}
-
-			if ev.Type == termbox.EventResize {
-				termbox.Flush()
-			}
-
-		case <-ticker.C:
-			FillArea(tbuffer, r, 'x', termbox.ColorCyan, termbox.ColorGreen, ALL)
-			BlitBuffer(b, tbuffer, x, y)
-			termbox.Flush()
-			x += dx
-			y += dy
-
-			if x > 60 || x <= 0 {
-				dx = -dx
-			}
-			if y > 30 || y <= 0 {
-				dy = -dy
+	gc.OnEvent = func(gc *GameCore, ev *termbox.Event) error {
+		if ev.Type == termbox.EventKey {
+			if ev.Ch == 'q' {
+				gc.DoQuit = true
 			}
 		}
+		return nil
+	}
+
+	gc.OnTick = func(gc *GameCore) error {
+		FillArea(gc.BackBuffer, r, 'x', termbox.ColorCyan, termbox.ColorGreen, ALL)
+		BlitBuffer(b, gc.BackBuffer, x, y)
+		x += dx
+		y += dy
+
+		if x > 60 || x <= 0 {
+			dx = -dx
+		}
+		if y > 30 || y <= 0 {
+			dy = -dy
+		}
+		return nil
+	}
+
+	err := gc.Start()
+	if err != nil {
+		panic(err)
 	}
 }
