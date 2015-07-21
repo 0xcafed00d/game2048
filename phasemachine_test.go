ad40e2edb606197d9777647db7716d158ebe30f5
Lee Witek <neomech@gmail.com>
2015-07-21 01:25:21 +0100

    added phasemachine tests

---
+++ phasemachine_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/simulatedsimian/assert"
+	"testing"
+	"time"
+)
+
+func TestPhaseMachine(t *testing.T) {
+	assert.FailFunc = t.Fatalf
+
+	pm := PhaseMachine{}
+
+	done := false
+
+	val := 0
+	count := 0
+	for !done {
+		pm.Begin()
+		if pm.Once() {
+			assert.Equal(t, val, 0)
+		}
+		if pm.Once() {
+			val++
+			assert.Equal(t, val, 1)
+		}
+		if pm.Manual() {
+			assert.True(t, count <= 5)
+			val++
+			count++
+			if count > 5 {
+				pm.Step()
+			}
+		}
+		if pm.Once() {
+			assert.Equal(t, val, 7)
+		}
+		if pm.Timed(time.Second * 3) {
+			val++
+		}
+		if pm.Once() {
+			assert.True(t, val > 30)
+			done = true
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
+}
