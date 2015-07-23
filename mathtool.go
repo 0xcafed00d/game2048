e776bd926a8a7c0bf47133dc3f5371d056278c0f
Lee Witek <neomech@gmail.com>
2015-07-23 01:48:30 +0100

    Added MapValue

--- mathtool.go
+++ mathtool.go
@@ -1,1 +1,9 @@
 package main
+
+func MapValue(val, srcMin, srcMax, dstMin, dstMax int64) int64 {
+	return (val-srcMin)*(dstMax-dstMin)/(srcMax-srcMin) + dstMin
+}
+
+func MapValuef(val, srcMin, srcMax, dstMin, dstMax float64) float64 {
+	return (val-srcMin)*(dstMax-dstMin)/(srcMax-srcMin) + dstMin
+}
