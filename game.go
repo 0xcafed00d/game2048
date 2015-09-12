8ab78a5ac293ba2d33adc5e6262a60be5502b3e8
Lee Witek <neomech@gmail.com>
2015-09-12 01:50:18 +0100

    adding gamestate

--- game.go
+++ game.go
@@ -1,57 +1,54 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/nsf/termbox-go"
 	"github.com/simulatedsimian/game2048/glib"
-	"github.com/simulatedsimian/rect"
+	//"github.com/simulatedsimian/rect"
 )
 
 func main() {
 	gc := glib.GameCore{}
 
-	var r rect.Rectangle
-	x, y := 0, 0
-	dx, dy := 1, 1
-
-	b := glib.MakeMemBuffer(10, 10)
-	glib.Fill(b, '0', termbox.ColorRed, termbox.ColorBlue, glib.ALL)
+	state := GameState{}
 
 	gc.OnInit = func(gc *glib.GameCore) error {
-		r = rect.WH(gc.BackBuffer.Size())
+		state.NewGame()
 		return nil
 	}
 
 	gc.OnEvent = func(gc *glib.GameCore, ev *termbox.Event) error {
 		if ev.Type == termbox.EventKey {
-			if ev.Ch == 'q' {
+
+			switch {
+			case ev.Ch == 'q' || ev.Ch == 'Q':
 				gc.DoQuit = true
+			case ev.Ch == 'r' || ev.Ch == 'R':
+				state.NewGame()
+			case ev.Ch == 'u' || ev.Ch == 'U':
+				state.UndoMove()
+			case ev.Key == termbox.KeyArrowLeft:
+				state.DoMove(Left)
+			case ev.Key == termbox.KeyArrowRight:
+				state.DoMove(Right)
+			case ev.Key == termbox.KeyArrowUp:
+				state.DoMove(Up)
+			case ev.Key == termbox.KeyArrowDown:
+				state.DoMove(Down)
 			}
 		}
 		return nil
 	}
 
 	gc.OnTick = func(gc *glib.GameCore) error {
-		glib.FillArea(gc.BackBuffer, r, 'x', termbox.ColorCyan, termbox.ColorGreen, glib.ALL)
-		glib.BlitBuffer(b, gc.BackBuffer, x, y)
-		x += dx
-		y += dy
-
-		if x > 60 || x <= 0 {
-			dx = -dx
-		}
-		if y > 30 || y <= 0 {
-			dy = -dy
-		}
-
-		DrawBox(gc.BackBuffer, rect.XYWH(0, 10, 9, 5), 0)
-		DrawBox(gc.BackBuffer, rect.XYWH(10, 10, 9, 5), 1)
-		DrawBox(gc.BackBuffer, rect.XYWH(20, 10, 9, 5), 2)
-		DrawBox(gc.BackBuffer, rect.XYWH(30, 10, 9, 5), 3)
+		state.Tick()
 		return nil
 	}
 
 	err := gc.Run()
 	if err != nil {
-		panic(err)
+		fmt.Fprintln(os.Stderr, err)
 	}
 }
--- gamestate.go
+++ gamestate.go
@@ -1,166 +1,24 @@
 package main
 
-import (
-	"fmt"
-)
-
-const (
-	BoardSize = 4
-)
-
-type Cell struct {
-	val    int
-	locked bool
-}
-
-// game board [0][0] is top left
-type GameBoard [BoardSize][BoardSize]Cell
-
-func printBoard(gb *GameBoard) {
-	for y := 0; y < BoardSize; y++ {
-		for x := 0; x < BoardSize; x++ {
-			fmt.Print(gb[x][y])
-		}
-		fmt.Println()
-	}
-	return
-}
-
-type Direction int
-
-const (
-	Up Direction = iota
-	Down
-	Left
-	Right
-)
-
-func DxDy(d Direction) (dx, dy int) {
-	switch d {
-	case Up:
-		dx, dy = 0, -1
-	case Down:
-		dx, dy = 0, 1
-	case Left:
-		dx, dy = -1, 0
-	case Right:
-		dx, dy = 1, 0
-	}
-	return
-}
+import ()
 
 type GameState struct {
 	Board     GameBoard
 	PrevBoard GameBoard
 }
 
-func RelativePos(x, y int, dir Direction) (newx, newy int, ok bool) {
-	dx, dy := DxDy(dir)
-	newx = x + dx
-	newy = y + dy
-	ok = newx >= 0 && newy >= 0 && newx < BoardSize && newy < BoardSize
+func (gs *GameState) NewGame() {
 
-	if !ok {
-		newx, newy = x, y
-	}
-
-	return
 }
 
-func (gb *GameBoard) MoveCell(x, y int, dir Direction) (moved bool, score int) {
-
-	if gb.CanMove(x, y, dir) {
-		newx, newy, _ := RelativePos(x, y, dir)
-		if gb[newx][newy].val == 0 {
-			gb[newx][newy] = gb[x][y]
-		} else {
-			gb[newx][newy].val += gb[x][y].val
-			gb[newx][newy].locked = true
-			score = gb[newx][newy].val
-		}
-		gb[x][y] = Cell{}
-		moved = true
-	}
-
-	return
+func (gs *GameState) DoMove(dir Direction) int {
+	return 0
 }
 
-func (gb *GameBoard) CanMove(x, y int, dir Direction) bool {
-	if gb[x][y].val == 0 {
-		return false
-	}
-
-	tox, toy, onBoard := RelativePos(x, y, dir)
-
-	fromCell := gb[x][y]
-	toCell := gb[tox][toy]
-
-	// can move if new location is on the board & not locked and destinaltion cell is empty
-	// or destination is same value as source
-	return onBoard && !toCell.locked && (toCell.val == 0 || toCell.val == fromCell.val)
+func (gs *GameState) UndoMove() int {
+	return 0
 }
 
-func (gb *GameBoard) Reset() {
-	*gb = GameBoard{}
-}
+func (gs *GameState) Tick() {
 
-func (gb *GameBoard) ClearLocks() {
-	for y := 0; y < BoardSize; y++ {
-		for x := 0; x < BoardSize; x++ {
-			gb[x][y].locked = false
-		}
-	}
-}
-
-type Move struct {
-	x, y int
-}
-
-func (gb *GameBoard) SingleStep(dir Direction) (moves []Move, score int) {
-
-	doMove := func(x, y int, dir Direction) {
-		moved, sc := gb.MoveCell(x, y, dir)
-		score += sc
-		if moved {
-			moves = append(moves, Move{x, y})
-		}
-	}
-
-	if dir == Left {
-		for y := 0; y < BoardSize; y++ {
-			for x := 0; x < BoardSize; x++ {
-				doMove(x, y, dir)
-			}
-		}
-	}
-
-	if dir == Right {
-		for y := 0; y < BoardSize; y++ {
-			for x := BoardSize - 1; x >= 0; x-- {
-				doMove(x, y, dir)
-			}
-		}
-	}
-
-	if dir == Up {
-		for x := 0; x < BoardSize; x++ {
-			for y := 0; y < BoardSize; y++ {
-				doMove(x, y, dir)
-			}
-		}
-	}
-
-	if dir == Down {
-		for x := 0; x < BoardSize; x++ {
-			for y := BoardSize - 1; y >= 0; y-- {
-				doMove(x, y, dir)
-			}
-		}
-	}
-
-	return
-}
-
-func (gb *GameBoard) FindFreeCell() (x int, y int, ok bool) {
-	return 0, 0, true
 }
